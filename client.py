96813f7bd06565ac1fcdb4a38e9046ddad871f51
blueisderp <lenduong2001@gmail.com>
2023-10-10 21:24:18 -0700

    changes

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ def info(packet_t, time_ms, src_ip, src_p, dest_ip, dest_p, pc, l, flags=None):
     print(f"Length: {l} bytes")
     if flags:
         print(f"Flags: {flags}")
-    print("----")
+    print("-------")
 
 def start_bc_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,14 +31,14 @@ def start_bc_client():
     client_socket.close()
 
 def start_mc_client():
-    MCAST_GRP = '224.1.1.1'
-    MCAST_PORT = 5007
+    mcast_group = '224.1.1.1'
+    mcast_port = 5007
     
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client_socket.bind((MCAST_GRP, MCAST_PORT))
+    client_socket.bind((mcast_group, mcast_port))
 
-    mreq = socket.inet_aton(MCAST_GRP) + socket.inet_aton('0.0.0.0')
+    mreq = socket.inet_aton(mcast_group) + socket.inet_aton('0.0.0.0')
     client_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     start_time = time.time() * 1000  # Start time in ms
@@ -46,7 +46,7 @@ def start_mc_client():
     end_time = time.time() * 1000  # End time in ms
     time_ms = end_time - start_time # time to receive message 
 
-    info('Multicast', time_ms, client_socket.getsockname()[0], MCAST_GRP, addr[0], MCAST_PORT, 'UDP', len(message))
+    info('Multicast', time_ms, client_socket.getsockname()[0], mcast_group, addr[0], mcast_port, 'UDP', len(message))
     print(f"Received multicast message: {message.decode()}")
 
 if __name__ == "__main__":
--- master.py
+++ master.py
@@ -5,10 +5,10 @@ def start_bc_master():
     host = '0.0.0.0'
     port = 12345
     master_socket.bind((host, port))
-    master_socket.listen(4)
+    master_socket.listen(4) #listening for up to 4 client nodes
     print(f"Listening for connections on {host}:{port}...")
 
-    clients = []  # Store clients
+    clients = []  # Store client nodes
 
     try:
         for i in range(4):
@@ -27,12 +27,12 @@ def start_bc_master():
         master_socket.close()
 
 def start_mc_master():
-    MCAST_GRP = '224.1.1.1'
-    MCAST_PORT = 5007
+    mcast_group = '224.1.1.1'
+    mcast_port = 5007
     master_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     message = b"Multicast message from the master!"
-    master_socket.sendto(message, (MCAST_GRP, MCAST_PORT))
+    master_socket.sendto(message, (mcast_group, mcast_port))
     master_socket.close()
 
 if __name__ == "__main__":
