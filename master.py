60c09220a698775554189a11d75b49c7e2470a8d
Christine <89506482+ChristineSulio@users.noreply.github.com>
2023-10-10 19:59:03 -0700

    Update master.py

--- master.py
+++ master.py
@@ -1,40 +1,40 @@
 import socket
 
-def start_broadcast_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_bc_master():
+    master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '0.0.0.0'
     port = 12345
-    server_socket.bind((host, port))
-    server_socket.listen(4)
+    master_socket.bind((host, port))
+    master_socket.listen(4)
     print(f"Listening for connections on {host}:{port}...")
 
     clients = []  # Store clients
 
     try:
         for i in range(4):
-            client_socket, addr = server_socket.accept()
+            client_socket, addr = master_socket.accept()
             print(f"Connection from {addr} established!")
             clients.append(client_socket)
 
         
         for client in clients:
-            message = b"Broadcast message from the server!"
+            message = b"Broadcast message from the master!"
             client.sendall(message)
 
     finally:
         for client in clients:
             client.close()
-        server_socket.close()
+        master_socket.close()
 
-def start_multicast_server():
+def start_mc_master():
     MCAST_GRP = '224.1.1.1'
     MCAST_PORT = 5007
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    master_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    message = b"Multicast message from the server!"
-    server_socket.sendto(message, (MCAST_GRP, MCAST_PORT))
-    server_socket.close()
+    message = b"Multicast message from the master!"
+    master_socket.sendto(message, (MCAST_GRP, MCAST_PORT))
+    master_socket.close()
 
 if __name__ == "__main__":
-    start_broadcast_server()
-    start_multicast_server()
+    start_bc_master()
+    start_mc_master()
