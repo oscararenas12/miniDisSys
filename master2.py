657c894b690e4df586d106d1561d403395cf23b4
el_o <72949326+oscararenas12@users.noreply.github.com>
2023-12-12 20:14:03 -0800

    Update master2.py

--- master2.py
+++ master2.py
@@ -1,13 +1,13 @@
 import socket
 import random
 
-def start_bc_master():
-    master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_bc_poll2():
+    poll2_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '0.0.0.0'
     port = 12346
-    master_socket.bind((host, port))
-    master_socket.listen(4)
-    master_socket.settimeout(10)  # Set a timeout for accepting connections
+    poll2_socket.bind((host, port))
+    poll2_socket.listen(4)
+    poll2_socket.settimeout(10)  # Set a timeout for accepting connections
 
     print(f"Listening for connections on {host}:{port}...")
     responses = {"yes": 0, "no": 0}
@@ -15,30 +15,30 @@ def start_bc_master():
     try:
         while True:
             try:
-                client_socket, addr = master_socket.accept()
+                node_socket, addr = poll2_socket.accept()
                 print(f"Connection from {addr} established!")
 
                 # Handle client interaction
-                poll_question = "Master 2 Do you like Python? (yes/no)"
-                client_socket.sendall(poll_question.encode())
-                response = client_socket.recv(1024).decode().lower()
+                poll_question = "(Poll 2) Do you like Cats? (yes/no)"
+                node_socket.sendall(poll_question.encode())
+                response = node_socket.recv(1024).decode().lower()
                 if response in responses:
                     responses[response] += 1
 
                 # Send final results to client
-                final_results = f"Master 2 Current Poll Results: Yes: {responses['yes']}, No: {responses['no']}"
-                client_socket.sendall(final_results.encode())
+                final_results = f"Current Poll 2 Results: Yes: {responses['yes']}, No: {responses['no']}"
+                node_socket.sendall(final_results.encode())
 
                 # Close client connection
-                client_socket.close()
+                node_socket.close()
 
             except socket.timeout:
                 break  # Break out of the loop if no new connections within timeout
 
     finally:
-        print("Master 2 Final Poll Results:")
+        print("Final Poll 2 Results:")
         print(f"Yes: {responses['yes']}, No: {responses['no']}")
-        master_socket.close()
+        poll1_socket.close()
 
 if __name__ == "__main__":
-    start_bc_master()
+    start_bc_poll2()
