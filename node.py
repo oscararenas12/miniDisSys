c191333f9789a169b4774bae00b9e8176ff62e85
el_o <72949326+oscararenas12@users.noreply.github.com>
2023-12-12 20:09:08 -0800

    Update and rename client.py to node.py

---
+++ node.py
@@ -0,0 +1,27 @@
+import socket
+import random
+
+def interact_with_poll(host, port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as node_socket:
+        node_socket.connect((host, port))
+
+        poll_question = node_socket.recv(1024).decode()
+        print(poll_question)
+
+        response = random.choice(["yes", "no"])
+        node_socket.sendall(response.encode())
+
+        final_results = node_socket.recv(1024).decode()
+        print(final_results)
+
+def start_bc_node():
+    poll_choice = random.choice(['poll1', 'poll2'])
+    print(f"Connecting to {poll_choice}")
+
+    if poll_choice == 'poll1':
+        interact_with_master('poll1', 12345)
+    else:
+        interact_with_master('poll2', 12346)
+
+if __name__ == "__main__":
+    start_bc_node()
