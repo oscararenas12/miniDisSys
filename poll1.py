38b9383f894052c7444bd085ae26842905419181
el_o <72949326+oscararenas12@users.noreply.github.com>
2023-12-12 20:11:16 -0800

    Update and rename master.py to poll1.py

--- poll1.py
+++ poll1.py
@@ -1,13 +1,13 @@
 import socket
 import random
 
-def start_bc_master():
-    master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def start_bc_poll1():
+    poll1_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = '0.0.0.0'
     port = 12345
-    master_socket.bind((host, port))
-    master_socket.listen(4)
-    master_socket.settimeout(10)  # Set a timeout for accepting connections
+    poll1_socket.bind((host, port))
+    poll1_socket.listen(4)
+    poll1_socket.settimeout(10)  # Set a timeout for accepting connections
 
     print(f"Listening for connections on {host}:{port}...")
     responses = {"yes": 0, "no": 0}
@@ -15,22 +15,22 @@ def start_bc_master():
     try:
         while True:
             try:
-                client_socket, addr = master_socket.accept()
+                node_socket, addr = poll1_socket.accept()
                 print(f"Connection from {addr} established!")
 
                 # Handle client interaction
                 poll_question = "Master 1 Do you like Python? (yes/no)"
-                client_socket.sendall(poll_question.encode())
-                response = client_socket.recv(1024).decode().lower()
+                node_socket.sendall(poll_question.encode())
+                response = node_socket.recv(1024).decode().lower()
                 if response in responses:
                     responses[response] += 1
 
                 # Send final results to client
                 final_results = f"Master 1 Current Poll Results: Yes: {responses['yes']}, No: {responses['no']}"
-                client_socket.sendall(final_results.encode())
+                node_socket.sendall(final_results.encode())
 
                 # Close client connection
-                client_socket.close()
+                node_socket.close()
 
             except socket.timeout:
                 break  # Break out of the loop if no new connections within timeout
@@ -38,7 +38,7 @@ def start_bc_master():
     finally:
         print("Master 1 Final Poll Results:")
         print(f"Yes: {responses['yes']}, No: {responses['no']}")
-        master_socket.close()
+        poll1_socket.close()
 
 if __name__ == "__main__":
-    start_bc_master()
+    start_bc_poll1()
